497b0708aba09b2d14528e7a9677c4880ea2fd76
crayon <crayon.zx@gmail.com>
2019-04-28 13:23:02 +0800

    Edited build script

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 const path = require('path');
 
-const OUT_DIR = '../types-antv-g6/lib';
-const CWD = path.resolve('../typed-g6');
+const OUT_DIR = 'types-antv-g6/lib';
+const CWD = path.resolve('..');
 
 exec(`npx tsc --outDir ${OUT_DIR}`, { cwd: CWD });
